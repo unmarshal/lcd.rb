ef764bc875cc31d7d2f63cb8dfb8e5f7ffa280e7
Marshall Allen Beddoe <unmarshal@gmail.com>
2015-04-28 15:51:19 -0700

    Make install.sh idempotent

--- install.sh
+++ install.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
-touch ~/.lcdrc
-echo "export LCD=`pwd`" >> ~/.lcd
-cat bash-aliases >> ~/.lcd
-echo "source ~/.lcd" >> ~/.bashrc
+grep "source ~/.lcd" ~/.bashrc
+FOUND=$?
 
-. ~/.bashrc
-
-acd lcd
-
-echo "Installed successfully. Reload your .bashrc file `. ~/.bashrc`"
+if [ $FOUND -ne 0 ]; then
+  touch ~/.lcdrc
+  echo "export LCD=`pwd`" >> ~/.lcd
+  cat bash-aliases >> ~/.lcd
+  echo "source ~/.lcd" >> ~/.bashrc
+  source ~/.lcd
+  echo "Installed successfully. Reload your .bashrc file `. ~/.bashrc`"
+else
+  echo "Already installed"
+fi
